59f91c69829918d35ca78357ca5745c2895f449c
carolinesilva1 <caroline_silva1@estudante.sc.senai.br>
2019-09-30 21:54:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,19 @@
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">  
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
     <style>
-    #alerta, #caixaSenha, #caixaRegistro{
-        display:none;
-    }
+        #alerta,
+        #caixaSenha,
+        #caixaRegistro {
+            display: none;
+        }
     </style>
 </head>
 
@@ -173,9 +175,29 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
+
     <script>
-    //Codigo jQuery para mostrar e ocultar os formulários
+        //Codigo jQuery para mostrar e ocultar os formulários
+        $(function() {
+            $("#btnEsqueci").click(function() {
+                $("#caixaLogin").hide(); //Ocultar
+                $("caixaSenha").show(); //Mostrar
+                $("#caixaRegistro").hide(); //Ocultar
+            });
+            $("#btnJaRegistrado").click(function() {
+                $("#caixaSenha").hide(); //Ocultar Gerar Senha
+                $("#caixaLogin").show(); //Mostrar Caixa Login
+            });
+
+            $("#btnJaRegistrado2").click(function() {
+                $("#caixaLogin").hide(); //Ocultar Gerar Senha
+                $("#caixaRegistro").show(); //Mostrar Caixa Login
+            });
+
+            $("#btnRegistrarNovo").click(function() {});
+            $("#caixaLogin").hide(); //Ocultar
+            $("caixaRegistro").show(); //Mostrar
+        });
     </script>
 </body>
 
