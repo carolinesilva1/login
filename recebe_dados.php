78945d6e835d74291e1324ae767d958321eaf94c
carolinesilva1 <caroline_silva1@estudante.sc.senai.br>
2019-10-07 21:31:59 -0300

    Envio de dados via AJAX e BD
    -Envio de dados dos dormularios de login e senha atraves do AJAX
    -Criação do banco de dados em arquivo SQL

---
+++ recebe_dados.php
@@ -0,0 +1,37 @@
+<?php
+
+    
+// Teste a ação 
+if(isset ($_POST['action'])){
+    if($_POST['action'] == 'cadastro'){
+//Teste se a ação é cadastro
+echo "\n<p> cadastro</p>";
+echo "\n<pre>";
+print_r ($_POST);
+echo "\n</pre>";
+
+}else if($_POST['action'] == 'login'){
+        //Senão teste se ação é login
+
+        echo "\n<p>login</p>";
+        echo "\n<pre>";
+        print_r($_POST);
+        echo "\n</pre>";
+
+}else if ($_POST['action'] == 'senha'){
+        //Senão teste se a açaõ é recuperar senha
+
+        echo "\n<p>senha</p>";
+        echo "\n<pre>";
+        print_r($_POST);
+        echo "\n</pre>";
+}else{
+        header("location:index.php");
+}
+
+
+}else{
+    //Redirecionando para index.php. negando o acesso
+    // a esse arquivo diretamente
+    header("location:index.php");
+}
