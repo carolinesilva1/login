333e9d3c0b7d1c0b6d7ebbe19744ad11dd12ffd0
carolinesilva1 <caroline_silva1@estudante.sc.senai.br>
2019-10-22 21:38:00 -0200

    perfil completo
    perfil.php preenchido com os dados da sessaõ

--- perfil.php
+++ perfil.php
@@ -33,7 +33,7 @@ require_once "session.php";
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Perfil
+                        <?= $nomeDoUsuario ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <a class="dropdown-item" href="#">Sobre</a>
@@ -48,13 +48,18 @@ require_once "session.php";
         <div class="container">
             <h1 class="display-4">Boas Vindas</h1>
             <h2 class="display-2 text-center bg-info rounded p-1 text-light">
-                Nome Completo
+            <?=    $nomeCompleto ?>
             </h2>
             <h2 class="text-center">
-                E-mail do Usuário:123@gmail.com
+                E-mail do Usuário:
+                <a href="mailto: <?=$emailUsuario?>">
+                <?=$emailUsuario?>
+
+                </a>
             </h2>
             <h2 class="text-center">
-                Registrado em 18/10/2019
+                Registrado em 18/10/2019 <?= $dataCriado =date("Y-m-d"); ?>
+
             </h2>
         </div>
     </div>
